3b2f4fdcc44f963108efbe812f5662b570354955
Matheus Hack <matheus@e-htl.com.br>
2018-03-28 14:38:59 -0300

    Initialize

---
+++ src/SefazSP/SefazSP.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matheus
+ * Date: 28/03/18
+ * Time: 14:34
+ */
+
+namespace MatheusHack\SefazSP;
+
+class SefazSP
+{
+
+}
---
+++ src/SefazSP/Soap/Client.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MatheusHack\SefazSP\Soap;
+
+
+abstract class RtsClient extends \SoapClient
+{
+
+    public function __construct($wsdl)
+    {
+        parent::__construct('' . $wsdl, [
+            'exceptions' => 0,
+            'trace' => 1,
+            'connection_timeout' => 1800,
+            'compression' =>  SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
+            'encoding' => 'UTF-8',
+            'cache_wsdl' => WSDL_CACHE_BOTH,
+        ]);
+    }
+
+}
